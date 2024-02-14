ddca56ca7c228c1b0a8e2954934149b089a74b09
Keith Wesa <61767651+kwesa@users.noreply.github.com>
2024-02-13 20:03:11 -0600

    Add files via upload

---
+++ C Projects/Math/Math.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    int a = 5;
+    int b = 8;
+    int c; 
+    c = sqrt(a * a + b * b);
+    printf("The hypotenuse of the triangle is %d", c);
+    return 0;
+}
